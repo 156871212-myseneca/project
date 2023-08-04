9c902bfe5d186d2d878742b1ee50f9315e50d39a
kkoshiya <kkoshiya@myseneca.ca>
2023-08-03 21:08:11 -0400

    Updated README.md

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -203,7 +203,7 @@ latlong=lat:long'
      identifier = "IoT device performance and failures"
 
      # if dataage - use:dataage_utcoffset_timetype
-     preprocesslogic='diff,min,variance,max,sum'
+     preprocesslogic='diff,min,variance,max,sum,trend,consistency'
      #preprocesslogic='dataage_-4_day,trend,min,max' # millisecond,second,minute,hour,day
      #preprocesslogic='dataage_-4_hour' # millisecond,second,minute,hour,day
 #     preprocesslogic='dataage_1_minute' # millisecond,second,minute,hour,day
